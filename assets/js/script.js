cf1b2071bf348248a6124c54fb8b5afcb7f005b1
Camm <arthurxx198@gmail.com>
2025-01-28 12:29:02 -0500

    v1-js-challenge

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+// References
+let searchBtn = document.querySelector("#search-btn"),
+  input = document.querySelector("#movie-search"),
+  movieResult = document.querySelector("#movies-results");
+
+searchBtn.addEventListener("click", () => {
+  let inputValue = input.value;
+  inputValue = `https://www.omdbapi.com/?t=${inputValue}&apikey=231b5f86`;
+  fetch(inputValue)
+    .then((res) => {
+      if (res.ok) {
+        return res.json();
+      }
+    })
+
+    .then((data) => {
+      data.Title;
+      data.Year;
+      data.Poster;
+
+      let img = document.createElement("img");
+      let dataa =
+        (movieResult.innerText = `${data.Title} ${data.Year}`);
+      img.src = `${data.Poster}`;
+      movieResult.appendChild(img, dataa);
+    })
+
+    .catch((err) => {
+      alert("Se ha producido un error :" + err.message);
+    });
+});
